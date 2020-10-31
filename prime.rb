745302aadb476b68d2a77ddde1caabd5c1741c40
Emmanuel <rivera128@live.com>
2020-10-31 01:57:38 -0500

    commit tree 00e0b06a1249177ac524ae94c48f5e80b51385e8

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(int)
+    if int == 1 || int < 1
+        return false
+    end
+    (2..(Math.sqrt(int))).each do |n|
+        return false if int % n == 0
+    end
+    true
+end
